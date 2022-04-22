6b1b3248732ea7c4d6293ee7289d6f3a65124983
weyprogramming <96354675+weyprogramming@users.noreply.github.com>
2022-04-22 03:19:04 +0200

    Update settings.py

--- inventory_web_app_django/inventory_web_app_django/settings.py
+++ inventory_web_app_django/inventory_web_app_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5bl!31k&_2qyx($xwzv1p5)4e#r2hkpwdh9gka#i^*dig2)890'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
